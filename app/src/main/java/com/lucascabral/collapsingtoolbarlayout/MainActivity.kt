db7609547f0bb2de4690e7b45e820594ffcd0ea6
Lucas Cabral <lucas_cabral.deve@outlook.com>
2021-10-05 14:54:33 -0300

    Date set to the current date of the device.

--- app/src/main/java/com/lucascabral/collapsingtoolbarlayout/MainActivity.kt
+++ app/src/main/java/com/lucascabral/collapsingtoolbarlayout/MainActivity.kt
@@ -2,10 +2,25 @@ package com.lucascabral.collapsingtoolbarlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.lucascabral.collapsingtoolbarlayout.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        setupViews()
+        binding.mainFab.setOnClickListener {
+
+        }
+    }
+
+    private fun setupViews() {
+        val currentTime = System.currentTimeMillis()
+        binding.mainDate.text = currentTime.getYearMonthDay()
     }
 }
